5fb796d7d7f93e161cc94599c784a48b45dff899
Dave Batiste <dave.batiste@d2l.com>
2016-01-28 16:34:44 -0500

    Updating encoding of .svg to be uri encoding instead of base-64, consistent with our other image processing.

--- create-scss.js
+++ create-scss.js
@@ -18,16 +18,27 @@ var writeScss = function(iconInfos, scssPath) {
 	scss.write('	vertical-align: text-top;\n');
 	scss.write('}\n');
 
+	var getContentValue = function(mimeType, icon) {
+		if (mimeType === 'image/png') {
+			return 'url("data:image/png;base64,' + icon.toString('base64') + '")';
+		} else if (mimeType === 'image/svg') {
+			return 'url("data:image/svg+xml,' + encodeURIComponent(icon.toString('utf8')) + '")';
+		} else {
+			deferred.reject(new Error('Unsupported mime/file type.'));
+			return;
+		}
+	};
+
 	for(var i=0; i<iconInfos.length; i++) {
 
 		var iconInfo = iconInfos[i];
 
 		scss.write('@mixin vui-icon-' + iconInfo.mixin + '() {\n');
 		scss.write('	&:before {\n');
-		scss.write('		content: url("data:' + iconInfo.mimeType + ';base64,' + iconInfo.ltrIcon.toString('base64') + '");\n');
+		scss.write('		content: ' + getContentValue(iconInfo.mimeType, iconInfo.ltrIcon) + ';\n');
 		if ( iconInfo.rtlIcon ) {
 			scss.write('		[dir=\'rtl\'] & {\n');
-			scss.write('			content: url("data:' + iconInfo.mimeType + ';base64,' + iconInfo.rtlIcon.toString('base64') + '");\n');
+			scss.write('			content: ' + getContentValue(iconInfo.mimeType, iconInfo.rtlIcon) + ';\n');
 			scss.write('		}\n');
 		}
 		scss.write('	}\n');
