d54c5cc71f6ddcf075750635bace65c8af13481d
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2018-11-09 15:56:37 -0500

    Polymer 3 Conversion

---
+++ d2l-icons.js
@@ -0,0 +1,7 @@
+import '../@polymer/polymer/polymer-legacy.js';
+import './d2l-icon.js';
+import './tier1-icons.js';
+import './tier2-icons.js';
+import './tier3-icons.js';
+import './html-editor-icons.js';
+import './emoji-icons.js';
